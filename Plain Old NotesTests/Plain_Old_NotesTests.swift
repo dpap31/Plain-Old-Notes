547042c444806f1b0b4dfcfb87fa6b51e9bbde05
Drew Pappas <drewpappas@mac.com>
2015-09-27 12:47:48 -0700

    Initial Commit

---
+++ Plain Old NotesTests/Plain_Old_NotesTests.swift
@@ -0,0 +1,36 @@
+//
+//  Plain_Old_NotesTests.swift
+//  Plain Old NotesTests
+//
+//  Created by Drew Pappas on 9/27/15.
+//  Copyright (c) 2015 Drew Pappas. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Plain_Old_NotesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
